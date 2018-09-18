f673f846e98a81643ec6051a786e0bd263c846c9
Pavel Kuzmin <mpakunderscore@gmail.com>
2018-09-18 22:00:27 +0300

    Some dirty code

--- server.js
+++ server.js
@@ -24,20 +24,85 @@ io.on('connection', (socket) => {
     // console.log(map);
 
     socket.emit('state', JSON.stringify(state));
-    //
-    // socket.on('sound', (message) => receiveSound(socket, message));
+
+    socket.on('up', (message) => up(socket, message));
+
+    socket.on('down', (message) => down(socket, message));
+
+    socket.on('siren', (message) => siren(socket, message));
+
+    socket.on('win', (message) => win(socket, message));
 
     // socket.on('disconnect', () => removeUser(socket));
 });
 
+function up(socket, message) {
+    tcpSocketOne.write('U');
+}
+
+function down(socket, message) {
+    tcpSocketOne.write('D');
+}
+
+function siren(socket, message) {
+
+    // на сообщение Z я включаю сирену, на сообщение Х выключаю (на каждом девайсе)
+
+    switch(message) {
+        case 1:
+            tcpSocket1.write('Z');
+            break;
+        case 2:
+            tcpSocket2.write('Z');
+            break;
+        case 3:
+            tcpSocket3.write('Z');
+            break;
+        case 4:
+            tcpSocket4.write('Z');
+            break;
+        default:
+            console.log("Siren error from " + message);
+    }
+
+    // TODO turn off after 10 sec
+    // tcpSocketOne.write('X');
+}
+
+function win(socket, message) {
+
+    switch(message) {
+        case 1:
+            tcpSocket1.write('Z');
+            break;
+        case 2:
+            tcpSocket1.write('Z');
+            break;
+        case 3:
+            tcpSocket1.write('Z');
+            break;
+        case 4:
+            tcpSocket1.write('Z');
+            break;
+        default:
+            console.log("Siren error from " + message);
+    }
+    // на сообщения 1-4 зажигаю на 101й девайс зажигаю цветом команды 1-4 подсветку
+}
+
 //TCP SOCKET
 let net = require('net');
 
-setInterval(function(){
+// setInterval(function(){
+//
+//     io.sockets.emit('state', JSON.stringify(state));
+//
+// }, 1000);
 
-    io.sockets.emit('state', JSON.stringify(state));
-
-}, 1000);
+let tcpSocket1 = null;
+let tcpSocket2 = null;
+let tcpSocket3 = null;
+let tcpSocket4 = null;
 
 let tcpServer = net.createServer(function(tcpSocket) {
 
@@ -55,7 +120,19 @@ let tcpServer = net.createServer(function(tcpSocket) {
             if (side >= 100 && side <= 110) {
 
                 state[side] = array.slice(0, 12);
-                console.log(state);
+                // console.log(state);
+
+                if (side === 101)
+                    tcpSocket1 = tcpSocket;
+
+                if (side === 102)
+                    tcpSocket1 = tcpSocket;
+
+                if (side === 103)
+                    tcpSocket1 = tcpSocket;
+
+                if (side === 104)
+                    tcpSocket1 = tcpSocket;
 
                 io.sockets.emit('state', JSON.stringify(state));
             }
@@ -72,3 +149,22 @@ let tcpServer = net.createServer(function(tcpSocket) {
 });
 
 tcpServer.listen(6060);
+
+
+
+//CLIENT
+
+// var client = new net.Socket();
+// client.connect(6060, '127.0.0.1', function() {
+//     console.log('Connected');
+//     client.write('Hello, server! Love, Client.');
+// });
+//
+// client.on('data', function(data) {
+//     console.log('Received: ' + data);
+//     client.destroy(); // kill client after server's response
+// });
+//
+// client.on('close', function() {
+//     console.log('Connection closed');
+// });
--- web/js/client.js
+++ web/js/client.js
@@ -4,17 +4,24 @@ function down() {
     socket.emit('down', '');
 }
 
+function up() {
+    socket.emit('up', '');
+}
+
+function siren(id) {
+    socket.emit('siren', id);
+    // на сообщение Z я включаю сирену, на сообщение Х выключаю (на каждом девайсе)
+}
+
+function win(id) {
+    socket.emit('win', id);
+    // на сообщения 1-4 зажигаю на 101й девайс зажигаю цветом команды 1-4 подсветку
+}
+
 socket.on('state', (state) => {
 
     console.log(JSON.parse(state));
-
     set(JSON.parse(state))
-
-    // console.log(JSON.parse(duck));
-
-    // map.npc.push(JSON.parse(duck));
-
-    // setUser();
 });
 
 function set(state) {
